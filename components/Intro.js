9665da5b83764bdaee6cf509285be0ebc2bc6310
Jay Cruz <49811999+CoderJay06@users.noreply.github.com>
2022-02-20 01:32:58 -0500

    Initial commit

---
+++ components/Intro.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Intro() {
+  return (
+    <div className="intro">
+      <div className="lemony-blob"></div>
+      <div className="intro-title">
+        <h1>Quizzical</h1>
+      </div>
+      <div className="intro-subtitle">
+        <p>Welcome</p>
+      </div>
+      <div className="ply-again-btn">
+        <button>Start Quiz</button>
+      </div>
+      <div className="baby-blob"></div>
+    </div>
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Intro from "../components/Intro";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Intro />
+    </div>
+  );
+}
